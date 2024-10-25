9e28fb718c9d88913ab01efb3116bbc8e37a266a
Mark Obad <mark.obad@gmail.com>
2024-10-24 20:08:14 -0700

    Increase ping interval

--- app/src/main/java/com/github/jing332/tts_server_android/model/speech/tts/EdgeTtsWS.kt
+++ app/src/main/java/com/github/jing332/tts_server_android/model/speech/tts/EdgeTtsWS.kt
@@ -52,7 +52,8 @@ class EdgeTtsWS : WebSocketListener() {
             .connectTimeout(SystemTtsConfig.requestTimeout.value.toLong(), TimeUnit.MILLISECONDS)
             .readTimeout(SystemTtsConfig.requestTimeout.value.toLong(), TimeUnit.MILLISECONDS)
             .writeTimeout(SystemTtsConfig.requestTimeout.value.toLong(), TimeUnit.MILLISECONDS)
-            .pingInterval(20, TimeUnit.MILLISECONDS)
+            .pingInterval(200, TimeUnit.MILLISECONDS)
+            .retryOnConnectionFailure(true)
             .build()
         ws = client.newWebSocket(req, this)
     }
